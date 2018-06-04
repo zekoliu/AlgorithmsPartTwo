52cf07f1c2f4cda1e4f43054d9556ec376240632
zekoliu <1393531285@qq.com>
2018-06-04 20:59:37 +0800

    add 4.4 CPM

---
+++ src/CPM.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class CPM {
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        StdIn.readLine();
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(2*N+2);
+        int s = 2*N, t = 2*N+1;
+        for (int i = 0; i < N; i++) {
+            String[] a = StdIn.readLine().split("\\s+");
+            double duration = Double.parseDouble(a[0]);
+            G.addEage(new DirectedEdge(i, i+N, duration));
+            G.addEage(new DirectedEdge(s, i, 0.0));
+
+            G.addEage(new DirectedEdge(i+N, t, 0.0));
+            for (int j = 0; j < a.length; j++) {
+                int successor = Integer.parseInt(a[j]);
+                G.addEage(new DirectedEdge(i+N, successor, 0.0));
+            }
+        }
+
+        AcyclicLP lp = new AcyclicLP(G, s);
+
+        StdOut.println("Start times:");
+        for (int i = 0; i < N; i++)
+            StdOut.printf("%4d: %5.1f\n", i, lp.distTo(i));
+        StdOut.printf("Finish time: %5.1f\n", lp.distTo(t));
+    }
+}
