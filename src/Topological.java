3ceb5aca4df3538e1bf3cbda6fcc61912b588a84
zekoliu <1393531285@qq.com>
2018-06-04 20:53:03 +0800

    rewrite Topological(order 4.3 AcyclicLP)

---
+++ src/Topological.java
@@ -0,0 +1,20 @@
+
+public class Topological {
+    private Iterable<Integer> order;
+
+    public Topological(EdgeWeightedDigraph G) {
+        DirectedCycle cyclefinder = new DirectedCycle(G);
+        if (!cyclefinder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
+        }
+    }
+
+    public Iterable<Integer> order() {
+        return order;
+    }
+
+    public boolean isDAG() {
+        return order != null;
+    }
+}
