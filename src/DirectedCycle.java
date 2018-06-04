5f306edc735d0ef6c8a0aa9ccc7340c35e97e514
zekoliu <1393531285@qq.com>
2018-06-04 20:52:21 +0800

    rewrite DirectedCycle(order 4.3 AcyclicLP)

---
+++ src/DirectedCycle.java
@@ -0,0 +1,48 @@
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class DirectedCycle {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private Stack<Integer> cycle;
+    private boolean[] onStack;
+
+    public DirectedCycle(EdgeWeightedDigraph G) {
+        onStack = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        marked = new boolean[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            if (!marked[v])
+                dfs(G, v);
+    }
+
+    public void dfs(EdgeWeightedDigraph G, int v) {
+        onStack[v] = true;
+        marked[v] = true;
+        for (DirectedEdge e : G.adj(v))
+            if (this.hasCycle()) return;
+            else if (!marked[e.to()]) {
+                edgeTo[e.to()] = v;
+                dfs(G, e.to());
+            }
+            else if (onStack[e.to()]) {
+                cycle = new Stack<Integer>();
+                for (int x = v; x != e.to(); x = edgeTo[x])
+                    cycle.push(x);
+                cycle.push(e.to());
+                cycle.push(v);
+            }
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+
+}
