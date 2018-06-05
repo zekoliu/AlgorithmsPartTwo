62718e6c326816adf14b753b11ce85d3138c5adb
zekoliu <1393531285@qq.com>
2018-06-05 10:38:24 +0800

    add 4.4 BellmanFordSP

---
+++ src/BellmanFordSP.java
@@ -0,0 +1,169 @@
+import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+
+public class BellmanFordSP {
+    private double[] distTo;
+    private DirectedEdge[] edgeTo;
+    private boolean[] onQ;
+    private Queue<Integer> queue;
+    private int cost;
+    private Iterable<DirectedEdge> cycle;
+
+    public BellmanFordSP(EdgeWeightedDigraph G, int s) {
+        distTo = new double[G.V()];
+        edgeTo = new DirectedEdge[G.V()];
+        onQ = new boolean[G.V()];
+        queue = new Queue<Integer>();
+
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+        onQ[s] = true;
+        queue.enqueue(s);
+        while (!queue.isEmpty() && !hasNegativeCycle()) {
+            int v = queue.dequeue();
+            onQ[v] = false;
+            relax(G, v);
+        }
+        assert check(G, s);
+    }
+
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[w] + e.weight();
+                edgeTo[w] = e;
+                if (!onQ[w]) {
+                    queue.enqueue(v);
+                    onQ[w] = true;
+                }
+            }
+            if (cost++ % G.V() == 0)
+                findNegativeCycle();
+            if (hasNegativeCycle()) return;
+        }
+    }
+
+    private void findNegativeCycle() {
+        int V = edgeTo.length;
+        EdgeWeightedDigraph spt = new EdgeWeightedDigraph(V);
+        for (int v = 0; v < V; v++)
+            if (edgeTo[v] != null)
+                spt.addEdge(edgeTo[v]);
+
+        EdgeWeightedDirectedCycle finder = new EdgeWeightedDirectedCycle(spt);
+        cycle = finder.cycle();
+    }
+
+    public boolean hasNegativeCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<DirectedEdge> negativeCycle() {
+        return cycle;
+    }
+
+    private boolean check(EdgeWeightedDigraph G, int s) {
+
+        // has a negative cycle
+        if (hasNegativeCycle()) {
+            double weight = 0.0;
+            for (DirectedEdge e : negativeCycle()) {
+                weight += e.weight();
+            }
+            if (weight >= 0.0) {
+                System.err.println("error: weight of negative cycle = " + weight);
+                return false;
+            }
+        }
+
+        // no negative cycle reachable from source
+        else {
+
+            // check that distTo[v] and edgeTo[v] are consistent
+            if (distTo[s] != 0.0 || edgeTo[s] != null) {
+                System.err.println("distanceTo[s] and edgeTo[s] inconsistent");
+                return false;
+            }
+            for (int v = 0; v < G.V(); v++) {
+                if (v == s) continue;
+                if (edgeTo[v] == null && distTo[v] != Double.POSITIVE_INFINITY) {
+                    System.err.println("distTo[] and edgeTo[] inconsistent");
+                    return false;
+                }
+            }
+
+            // check that all edges e = v->w satisfy distTo[w] <= distTo[v] + e.weight()
+            for (int v = 0; v < G.V(); v++) {
+                for (DirectedEdge e : G.adj(v)) {
+                    int w = e.to();
+                    if (distTo[v] + e.weight() < distTo[w]) {
+                        System.err.println("edge " + e + " not relaxed");
+                        return false;
+                    }
+                }
+            }
+
+            // check that all edges e = v->w on SPT satisfy distTo[w] == distTo[v] + e.weight()
+            for (int w = 0; w < G.V(); w++) {
+                if (edgeTo[w] == null) continue;
+                DirectedEdge e = edgeTo[w];
+                int v = e.from();
+                if (w != e.to()) return false;
+                if (distTo[v] + e.weight() != distTo[w]) {
+                    System.err.println("edge " + e + " on shortest path not tight");
+                    return false;
+                }
+            }
+        }
+
+        StdOut.println("Satisfies optimality conditions");
+        StdOut.println();
+        return true;
+    }
+
+    public double distTo(int v) {
+        if (hasNegativeCycle())
+            throw new UnsupportedOperationException("Negative cost cycle exists");
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
+            path.push(e);
+        return path;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int s = Integer.parseInt(args[1]);
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+
+        BellmanFordSP sp = new BellmanFordSP(G, s);
+
+        if (sp.hasNegativeCycle()) {
+            for (DirectedEdge e : sp.negativeCycle())
+                StdOut.println(e);
+        } else {
+            for (int v = 0; v < G.V(); v++) {
+                if (sp.hasPathTo(v)) {
+                    StdOut.printf("%d to %d (%5.2f)  ", s , v, sp.distTo[v]);
+                    for (DirectedEdge e : sp.pathTo(v)) {
+                        StdOut.print(e + "    ");
+                    }
+                    StdOut.println();
+                } else {
+                    StdOut.printf("%d to %d            no path\n", s, v);
+                }
+            }
+        }
+    }
+}
